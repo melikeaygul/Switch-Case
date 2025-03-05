436e62029d7fa4d1d3e6a846097d9f1a6998bb8e
melikeaygul <melikeaygul0810@gmail.com>
2025-03-05 15:57:17 +0100

    Aufgabe2

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 enum Weekday { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
 
+//Aufgabe 1
 void main() {
   int weekday = 5;
   String weekdayTxt = '';
@@ -20,4 +21,32 @@ void main() {
       weekdayTxt = 'Wochenende ist Genial';
   }
   print(weekdayTxt);
+
+  // Aufgabe 2
+
+  String sex = 'female';
+  int age = 21;
+
+  switch (sex) {
+    case 'male':
+      switch (age) {
+        case >= 20 && <= 25:
+          print("Im Schnitt 181,4m");
+        case > 25 && <= 30:
+          print("Im Schnitt 181,3m");
+        case > 30 && < 35:
+          print("Im Schnitt 180,4m");
+      }
+    case 'female':
+      switch (age) {
+        case >= 20 && <= 25:
+          print("Im Schnitt 167,5m");
+        case > 25 && <= 30:
+          print("Im Schnitt 167,3 m");
+        case > 30 && <= 35:
+          print("Im Schnitt 167,2");
+      }
+  }
 }
+// Ich habe das mit Markus gemacht, er hatte sich mit Melli zsm. gesetzt.
+//Habe es nur halb verstanden, werde es mir nochmal extra angucken
